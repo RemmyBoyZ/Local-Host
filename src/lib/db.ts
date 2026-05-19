7f9a739f5f9bd02ea2f0ce69fe058f13f28ee860
RemmyBoyZ <ahmadfadhilr72@gmail.com>
2026-05-19 13:51:28 +0700

    -

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,9 +1,8 @@
-// src/lib/prisma.ts
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+const globalForPrisma = globalThis as unknown as { db: PrismaClient };
 
-export const prisma =
-  globalForPrisma.prisma || new PrismaClient();
+export const db =
+  globalForPrisma.db || new PrismaClient();
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== "production") globalForPrisma.db = db;
